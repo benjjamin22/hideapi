c40ea9293457b9f1817f240c7683043c1ef53a85
benjamin <deeprising4ilife@gmail.com>
2024-03-11 10:30:42 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const url = require('url')
+const express = require('express')
+const router = express.Router()
+const needle = require('needle')
+const apicache = require('apicache')
+
+// Env vars
+
+const API_BASE_URL = process.env.API_BASE_URL
+
+// Init cache
+let cache = apicache.middleware
+
+router.get('/', cache('2 minutes'), async(req, res, next) => {
+
+    try {
+        const apiRes = await needle('get', `${API_BASE_URL}`)
+        const data = apiRes.body
+
+        res.status(200).json(data)
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = router
